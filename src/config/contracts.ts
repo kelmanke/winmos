d6ea914daa0bbdfed8870c8e79069dc09dc803f1
kelmanke <teariart@gmail.com>
2023-05-28 16:29:13 +0300

    change address

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -2,8 +2,8 @@ import { Address } from 'wagmi'
 import lottery from './abi/lottery.json'
 import winners from './abi/winners.json'
 
-const lotteryContractAddr: Address = '0x5a0cD5B73e2c7c908d4c0E74299aee8dF1D3A05f'
-const winnersContractAddr: Address = '0x14FEF717bB704C0054E73Dc98676831CD5b569C2'
+const lotteryContractAddr: Address = '0xf42a3F261a0E122Fb7F01a5296F727EA86cF7701'
+const winnersContractAddr: Address = '0x0b4E16E348A6396b4337b42BCFc19dF787A64454'
 const lotteryContractAbi = lottery
 const winnersContractABI = winners
 
